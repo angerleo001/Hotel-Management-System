d12cfa9cd02c7a4e6b471e7e8356c38ab452e527
Anger Leo <105979964+angerleo001@users.noreply.github.com>
2023-08-26 08:28:49 +0545

    Login

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,11 @@
-public class Login {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Login extends JFrame implements ActionListener{
     Login(){
+
+    }
+    public void ActionListener(ActionListener ae){
 
     }
     public static void main(String[] arg){
