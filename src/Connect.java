f4f9ff1f95673a2be5f878691107eb739f37968a
Anger Leo <105979964+angerleo001@users.noreply.github.com>
2023-08-27 23:33:41 +0545

    Sql Connector

--- src/Connect.java
+++ src/Connect.java
@@ -1,6 +1,10 @@
 public class Connect {
-    public Connect(){
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public Connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
     public static void main(String[] arg){
 
