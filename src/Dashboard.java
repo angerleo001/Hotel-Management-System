abdba746f9ad0399ae0d3a53ece54ced91915960
Anger Leo <105979964+angerleo001@users.noreply.github.com>
2023-08-27 23:07:09 +0545

    V1.2

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Dashboard extends JFrame {
-    JLabel img3, l4;
+    JLabel img3, l4, l10;
     JMenuBar m1;
     JMenu mb1;
 
@@ -20,29 +20,31 @@ public class Dashboard extends JFrame {
         img3.setBounds(0, 0, 1366, 768);
         add(img3);
 
+        l10 = new JLabel("Welcome TO System");
+        l10.setForeground(Color.CYAN);
+        l10.setBounds(470,100,450,50);
+        l10.setFont(new Font("Tahoma",Font.BOLD,40));
+        img3.add(l10);
+
+
         // Labels
         l4 = new JLabel("Hotel Management System APS");
         l4.setBounds(15, 650, 300, 50);
         l4.setFont(new Font("Times New Roman", Font.BOLD, 15));
-        l4.setForeground(Color.BLUE);
+        l4.setForeground(Color.BLACK);
         img3.add(l4);
 
         //Menu Bar
         m1 = new JMenuBar();
         m1.setBounds(0,0,1366, 30);
+        m1.setBackground(Color.GREEN);
+        m1.setForeground(Color.GREEN);
         img3.add(m1);
 
         //Menu
         mb1 = new JMenu("Hotel Management");
-        mb1.setForeground((Color.CYAN));
-        mb1.setBackground(Color.blue);
+        mb1.setForeground((Color.BLACK));
         m1.add(mb1);
-
-        //Images
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/landscape-Sun.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1950,1000,Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-
     }
 
     public static void main(String[] args) {
