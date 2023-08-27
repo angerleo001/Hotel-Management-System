2765c0a328f44beacef98bc1f3b442fe4672a326
Anger Leo <105979964+angerleo001@users.noreply.github.com>
2023-08-27 23:36:45 +0545

    Update Connect.java

--- src/Connect.java
+++ src/Connect.java
@@ -1,7 +1,11 @@
+import java.sql.*;
+
 public class Connect {
+    Connection c;
     public Connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager;
         }catch (Exception e){
             e.printStackTrace();
         }
